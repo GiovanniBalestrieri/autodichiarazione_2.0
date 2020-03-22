346d3c5ab1263670253466e40c37e2e3c547c3d6
Giovanni <giovanni.balestrieri@inria.fr>
2020-03-22 18:22:38 +0100

    Ok we have the sequence of geopoint of the directions

--- path_planning/basic_example.py
+++ path_planning/basic_example.py
@@ -1,18 +1,25 @@
 import openrouteservice
 from openrouteservice import convert
 
+# Change your token here
 token = '5b3ce3597851110001cf62488a48a7f1e3ad491184353a3b070c32a2'
 
+# Setting POIs
+home =  (5.710752, 45.178611) 
+market_1 = (5.709025, 45.175798)
+work_spot = (5.807179, 45.216604)
+ 
+# Create coordinates tuple
+market_one_way = (home,market_1)
+market_round_trip = (home, market_1, home)
+work_round_trip = (home, work_spot, home)
 
-coords = ((8.34234,48.23424),(8.34423,48.26424))
-
-client = openrouteservice.Client(key=token) # Specify your personal API key
-#routes = client.directions(coords)
+# Connect to client
+client = openrouteservice.Client(key=token) # Specify your token
 
 # decode_polyline needs the geometry only
-geometry = client.directions(coords)['routes'][0]['geometry']
-
+geometry = client.directions(market_one_way)['routes'][0]['geometry']
 decoded = convert.decode_polyline(geometry)
 
-print(decoded)
+print(decoded['coordinates'])
 
---
+++ path_planning/readme.md
@@ -0,0 +1,9 @@
+# Path Planning Module
+
+Aim1: Get direction from source to destination points
+Aim2: Decompose the path in a sequence of coordinates 
+
+# Resources
+
+Wiki developer:
+https://openrouteservice-py.readthedocs.io/en/latest/
