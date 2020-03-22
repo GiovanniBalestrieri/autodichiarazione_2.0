308aa52f3af5f54b3f2e482083d0437c94868290
Giovanni <giovanni.balestrieri@inria.fr>
2020-03-22 17:26:37 +0100

    Ok, by default, the directions API returns encoded polylines, To decode to a dict, which is a GeoJSON geometry object, simply use the convert function provided

--- path_planning/basic_example.py
+++ path_planning/basic_example.py
@@ -1,13 +1,18 @@
 import openrouteservice
+from openrouteservice import convert
+
 token = '5b3ce3597851110001cf62488a48a7f1e3ad491184353a3b070c32a2'
 
 
 coords = ((8.34234,48.23424),(8.34423,48.26424))
 
 client = openrouteservice.Client(key=token) # Specify your personal API key
-routes = client.directions(coords)
+#routes = client.directions(coords)
 
-print(routes)
-print("\n\nAAAA\n\n")
-print(routes['routes'][0]['geometry'])
+# decode_polyline needs the geometry only
+geometry = client.directions(coords)['routes'][0]['geometry']
+
+decoded = convert.decode_polyline(geometry)
+
+print(decoded)
 
