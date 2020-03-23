66c98d744023e94855b58fe9519ed6dc68998eed
Giovanni <giovanni.balestrieri@inria.fr>
2020-03-23 22:53:57 +0100

    ok milestone 1

--- consistency_check/utils.py
+++ consistency_check/utils.py
@@ -16,20 +16,23 @@ class Utilities:
     def distance(self, pt1, pt2):
         return np.sqrt( (pt2[0]-pt1[0])**2 +(pt2[1] - pt1[1])**2)
 
-    def explode_segment(self, from_pt, to_pt, inter_distance=0.01):
+    def explode_segment(self, from_pt, to_pt, inter_distance=0.03):
         """ Creates a number of intermediate points from start to end
         """
         pt = []
         distance = self.distance(from_pt,to_pt)
         num_inter_points = int(distance/inter_distance)
+        from_x = np.array([from_pt[0],from_pt[1]])
+        to_x = np.array([to_pt[0],to_pt[1]])
+        if num_inter_points == 0:
+            pt.append(from_pt)
         if not distance%inter_distance == 0:
             rest = distance - num_inter_points*inter_distance
         for i in range(num_inter_points):
-            from_x = np.array([from_pt[0],from_pt[1]])
-            to_x = np.array([to_pt[0],to_pt[1]])
             if i == 0:
                 # Check that maybe just pt.append(from_pt) should be ok
                 pt.append(self.create_inter_points(from_x,to_x,i*(1.0/num_inter_points)))
+                #pt.append(from_x)
             else:
                 pt.append(self.create_inter_points(from_x,to_x,i*(inter_distance/distance)))
         return pt
@@ -45,3 +48,8 @@ class Utilities:
         XX = np.radians(float(coord[0])-float(self.ref_point[0]))*6371*np.cos(np.radians(float(coord[1])))
         YY = np.radians(float(coord[1])-float(self.ref_point[1]))*6371
         return (XX,YY)
+
+    def closest_node(self, node, nodes):
+        nodes = np.asarray(nodes)
+        dist_2 = np.sum((nodes - node)**2, axis=1)
+        return np.argmin(dist_2)
