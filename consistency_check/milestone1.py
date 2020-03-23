f6b2613ed653930cb2a22e3a5d0a95e1f586e420
Giovanni <giovanni.balestrieri@inria.fr>
2020-03-23 19:49:37 +0100

    Added basic script mileston1

--- consistency_check/milestone1.py
+++ consistency_check/milestone1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from utils import Utilities
+import matplotlib.pyplot as plt
 import json
 
 # Load sequence of gps positions
@@ -8,17 +9,28 @@ with open('path.json') as f:
 
 utils = Utilities(data[0])
 
+
 projected_coord = []
+projected_coord_extended = []
 
 # Project coordinatesa
 for idx,i in enumerate(data):
 	if idx == 0:
-		pass
+		projected_coord.append(utils.project(i))
+		continue
 	print(str(idx) + " : " + str(i)) 
-	projected_coord.extend(utils.explode_segment(utils.project(data[idx-1]), utils.project(i)))
+	projected_coord.append(utils.project(i))
+	projected_coord_extended.extend(utils.explode_segment(utils.project(data[idx-1]), utils.project(i)))
+
+print(projected_coord)
+
+for i in projected_coord: 
+	print(i)
+	plt.plot(i[0],i[1],'ro')
 
 
-print(len(data))
-print(len(projected_coord))
+for i in projected_coord_extended: 
+	plt.plot(i[0],i[1],'bo')
 
-
+plt.grid()
+plt.show()
